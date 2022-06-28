d02f0519ddee537c2764bce3983a32c3270c908c
Jake Chien <jake_chien@outlook.com>
2022-06-28 03:43:55 -0300

    Requisito 07 :thumbsup:

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -179,6 +179,21 @@ class Wallet extends React.Component {
             onClick={ this.handleClick }
           />
         </form>
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Descrição</th>
+              <th scope="col">Tag</th>
+              <th scope="col">Método de pagamento</th>
+              <th scope="col">Valor</th>
+              <th scope="col">Moeda</th>
+              <th scope="col">Câmbio utilizado</th>
+              <th scope="col">Valor convertido</th>
+              <th scope="col">Moeda de conversão</th>
+              <th scope="col">Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
       </>
     );
   }
